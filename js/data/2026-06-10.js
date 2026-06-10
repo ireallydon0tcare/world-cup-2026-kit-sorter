912ca6f90b4661cbe14f92b4a2143ae953eebff7
ireallydon0tcare <hxmaierjp@gmail.com>
2026-06-10 21:16:54 +0200

    Add files via upload

---
+++ js/data/2026-06-10.js
@@ -0,0 +1,700 @@
+// dataSetVersion = "2019-11-26"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSetVersion = "2026-06-10";
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].options = [
+  {
+    name: "Home Kits",
+    key: "home"
+  },
+  {
+    name: "Away Kits",
+    key: "away",
+    checked: false
+  },
+];
+
+dataSet[dataSetVersion].characterData = [
+  {
+    name: "Algeria Home",
+    img: "VpqbcKkf/algeria-2026-home-kit.jpg",
+    opts: {
+        key: "home"
+      }
+  },
+  {
+    name: "Argentina Home",
+    img: "3ZLzVV1/argentina-2026-home-kit.jpg",
+    opts: {
+        key: "home"
+      }
+  },
+  {
+    name: "Australia Home",
+    img: "FkRgnY4F/australia-2026-home-kit.jpg",
+    opts: {
+        key: "home"
+      }
+  },
+  {
+    name: "Austria Home",
+    img: "TqDrQfMm/austria-2026-home-kit.jpg",
+    opts: {
+        key: "home"
+      }
+  },
+  {
+    name: "Belgium Home",
+    img: "p6Hf5K8P/belgium-2026-home-kit.jpg",
+    opts: {
+        key: "home"
+      }
+  },
+  {
+    name: "Bosnia and Herzegovina Home",
+    img: "xSd85pDT/bosnia-and-herzegovina-2026-home-kit.jpg",
+    opts: {
+        key: "home"
+      }
+  },
+  {
+    name: "Brazil Home",
+    img: "7scW3DS/brazil-2026-home-kit.jpg",
+    opts: {
+        key: "home"
+      }
+  },
+  {
+    name: "Canada Home",
+    img: "7xd7yNBg/canada-2026-home-kit.jpg",
+    opts: {
+        key: "home"
+      }
+  },
+  {
+    name: "Cape Verde Home",
+    img: "Kcd8rc1T/cape-verde-2026-home-kit.jpg",
+    opts: {
+        key: "home"
+      }
+  },
+  {
+    name: "Colombia Home",
+    img: "tpYYFfgp/colombia-2026-home-kit.jpg",
+    opts: {
+        key: "home"
+      }
+  },
+  {
+    name: "Croatia Home",
+    img: "7t6LZZrB/croatia-2026-home-kit.jpg",
+    opts: {
+        key: "home"
+      }
+  },
+  {
+    name: "Curacao Home",
+    img: "1t0916x1/curacao-2026-home-kit.jpg",
+    opts: {
+        key: "home"
+      }
+  },
+  {
+    name: "Czech Republic Home",
+    img: "SwcQ7vnq/czech-republic-2026-home-kit.jpg",
+    opts: {
+        key: "home"
+      }
+  },
+  {
+    name: "DR Congo Home",
+    img: "Q7DyGSWV/democratic-republic-of-the-congo-2026-home-kit.jpg",
+    opts: {
+        key: "home"
+      }
+  },
+  {
+    name: "Ecuador Home",
+    img: "V0p0VB1k/ecuador-2026-home-kit.jpg",
+    opts: {
+        key: "home"
+      }
+  },
+  {
+    name: "Egypt Home",
+    img: "ycvJYXqK/egypt-2026-home-kit.jpg",
+    opts: {
+        key: "home"
+      }
+  },
+  {
+    name: "England Home",
+    img: "J8TGskY/england-2026-home-kit.jpg",
+    opts: {
+        key: "home"
+      }
+  },
+  {
+    name: "France Home",
+    img: "FSFP1b4/france-2026-home-kit.jpg",
+    opts: {
+        key: "home"
+      }
+  },
+  {
+    name: "Germany Home",
+    img: "CK6z9qRT/germany-2026-home-kit.jpg",
+    opts: {
+        key: "home"
+      }
+  },
+  {
+    name: "Ghana Home",
+    img: "tMdHy874/ghana-2026-home-kit.jpg",
+    opts: {
+        key: "home"
+      }
+  },
+  {
+    name: "Haiti Home",
+    img: "rRg3FbRS/haiti-2026-home-kit.jpg",
+    opts: {
+        key: "home"
+      }
+  },
+  {
+    name: "Iran Home",
+    img: "BHkLZC9R/iran-2026-home-kit.jpg",
+    opts: {
+        key: "home"
+      }
+  },
+  {
+    name: "Iraq Home",
+    img: "F1FkKzP/iraq-2026-home-kit.jpg",
+    opts: {
+        key: "home"
+      }
+  },
+  {
+    name: "Ivory Coast Home",
+    img: "vvqm4d2F/ivory-coast-2026-home-kit.jpg",
+    opts: {
+        key: "home"
+      }
+  },
+  {
+    name: "Japan Home",
+    img: "B5mjV594/japan-2026-home-kit.jpg",
+    opts: {
+        key: "home"
+      }
+  },
+  {
+    name: "Jordan Home",
+    img: "Tx9LSnwg/jordan-2026-home-kit.jpg",
+    opts: {
+        key: "home"
+      }
+  },
+  {
+    name: "Mexico Home",
+    img: "wFn0RTzx/mexico-2026-home-kit.jpg",
+    opts: {
+        key: "home"
+      }
+  },
+  {
+    name: "Morocco Home",
+    img: "gbhytyYF/morocco-2026-home-kit.jpg",
+    opts: {
+        key: "home"
+      }
+  },
+  {
+    name: "Netherlands Home",
+    img: "VYq68WXC/netherlands-2026-home-kit.jpg",
+    opts: {
+        key: "home"
+      }
+  },
+  {
+    name: "New Zealand Home",
+    img: "zVbKznjX/new-zealand-2026-home-kit.jpg",
+    opts: {
+        key: "home"
+      }
+  },
+  {
+    name: "Norway Home",
+    img: "yFBKpM50/norway-2026-home-kit.jpg",
+    opts: {
+        key: "home"
+      }
+  },
+  {
+    name: "Panama Home",
+    img: "v6jgjhYP/panama-2026-home-kit.jpg",
+    opts: {
+        key: "home"
+      }
+  },
+  {
+    name: "Paraguay Home",
+    img: "Fb2WJJpn/paraguay-2026-home-kit.jpg",
+    opts: {
+        key: "home"
+      }
+  },
+  {
+    name: "Portugal Home",
+    img: "4n7pxykQ/portugal-2026-home-kit.jpg",
+    opts: {
+        key: "home"
+      }
+  },
+  {
+    name: "Qatar Home",
+    img: "ch1WqRDh/qatar-2026-home-kit.jpg",
+    opts: {
+        key: "home"
+      }
+  },
+  {
+    name: "Saudi Arabia Home",
+    img: "3yLq2v6R/saudi-arabia-2026-home-kit.jpg",
+    opts: {
+        key: "home"
+      }
+  },
+  {
+    name: "Scotland Home",
+    img: "N62M366R/scotland-2026-home-kit.jpg",
+    opts: {
+        key: "home"
+      }
+  },
+  {
+    name: "Senegal Home",
+    img: "BHV7WY8t/senegal-2026-home-kit.jpg",
+    opts: {
+        key: "home"
+      }
+  },
+  {
+    name: "South Africa Home",
+    img: "svCsBZDP/south-africa-2026-home-kit.jpg",
+    opts: {
+        key: "home"
+      }
+  },
+  {
+    name: "South Korea Home",
+    img: "qLfmQPzt/south-korea-2026-home-kit.jpg",
+    opts: {
+        key: "home"
+      }
+  },
+  {
+    name: "Spain Home",
+    img: "zH6444z2/spain-2026-home-kit.jpg",
+    opts: {
+        key: "home"
+      }
+  },
+  {
+    name: "Sweden Home",
+    img: "Vf2Bdqs/sweden-2026-home-kit.jpg",
+    opts: {
+        key: "home"
+      }
+  },
+  {
+    name: "Switzerland Home",
+    img: "jk7NW7m4/switzerland-2026-home-kit.jpg",
+    opts: {
+        key: "home"
+      }
+  },
+  {
+    name: "Tunisia Home",
+    img: "bjgVNP8f/tunisia-2026-home-kit.jpg",
+    opts: {
+        key: "home"
+      }
+  },
+  {
+    name: "Turkey Home",
+    img: "C3XgjZzg/turkiye-2026-home-kit.jpg",
+    opts: {
+        key: "home"
+      }
+  },
+  {
+    name: "Uruguay Home",
+    img: "fVgZ2VV8/uruguay-2026-home-kit.jpg",
+    opts: {
+        key: "home"
+      }
+  },
+  {
+    name: "USA Home",
+    img: "DPXT16kR/usa-2026-home-kit.jpg",
+    opts: {
+        key: "home"
+      }
+  },
+  {
+    name: "Uzbekistan Home",
+    img: "4ZDj2ysh/uzbekistan-2026-home-kit.jpg",
+    opts: {
+        key: "home"
+      }
+  },
+
+/**
+ *
+ * 
+ * 
+ * 
+ * 
+ *  
+ */
+
+  {
+    name: "Algeria Away",
+    img: "5hmwT8p7/algeria-2026-away-kit.jpg",
+    opts: {
+        key: "away"
+      }
+  },
+  {
+    name: "Argentina Away",
+    img: "gFJDZGJR/argentina-2026-away-kit.jpg",
+    opts: {
+        key: "away"
+      }
+  },
+  {
+    name: "Australia Away",
+    img: "ynhQBD1B/australia-2026-away-kit.jpg",
+    opts: {
+        key: "away"
+      }
+  },
+  {
+    name: "Austria Away",
+    img: "xK8CcNhD/austria-2026-away-kit.jpg",
+    opts: {
+        key: "away"
+      }
+  },
+  {
+    name: "Belgium Away",
+    img: "dJPNWk0p/belgium-2026-away-kit.jpg",
+    opts: {
+        key: "away"
+      }
+  },
+  {
+    name: "Bosnia and Herzegovina Away",
+    img: "1G1HNM9Z/bosnia-and-herzegovina-2026-away-kit.jpg",
+    opts: {
+        key: "away"
+      }
+  },
+  {
+    name: "Brazil Away",
+    img: "pjhg56YF/brazil-2026-away-kit.jpg",
+    opts: {
+        key: "away"
+      }
+  },
+  {
+    name: "Canada Away",
+    img: "x8M5DN8b/canada-2026-away-kit.jpg",
+    opts: {
+        key: "away"
+      }
+  },
+  {
+    name: "Cape Verde Away",
+    img: "yFXpxzy7/cape-verde-2026-away-kit.jpg",
+    opts: {
+        key: "away"
+      }
+  },
+  {
+    name: "Colombia Away",
+    img: "CF6XXHR/colombia-2026-away-kit.jpg",
+    opts: {
+        key: "away"
+      }
+  },
+  {
+    name: "Croatia Away",
+    img: "B2C8DV7T/croatia-2026-away-kit.jpg",
+    opts: {
+        key: "away"
+      }
+  },
+  {
+    name: "Curacao Away",
+    img: "YTDK9K4v/curacao-2026-away-kit.jpg",
+    opts: {
+        key: "away"
+      }
+  },
+  {
+    name: "Czech Republic Away",
+    img: "4Z54JTzX/czech-republic-2026-away-kit.jpg",
+    opts: {
+        key: "away"
+      }
+  },
+  {
+    name: "DR Congo Away",
+    img: "Q56P71p/democratic-republic-of-the-congo-2026-away-kit.jpg",
+    opts: {
+        key: "away"
+      }
+  },
+  {
+    name: "Ecuador Away",
+    img: "Z65S0KnG/ecuador-2026-away-kit.jpg",
+    opts: {
+        key: "away"
+      }
+  },
+  {
+    name: "Egypt Away",
+    img: "PZrpGjw6/egypt-2026-away-kit.jpg",
+    opts: {
+        key: "away"
+      }
+  },
+  {
+    name: "England Away",
+    img: "rRTjsCFH/england-2026-away-kit.jpg",
+    opts: {
+        key: "away"
+      }
+  },
+  {
+    name: "France Away",
+    img: "PvcvX2L4/france-2026-away-kit.jpg",
+    opts: {
+        key: "away"
+      }
+  },
+  {
+    name: "Germany Away",
+    img: "C3HRQsjR/germany-2026-away-kit.jpg",
+    opts: {
+        key: "away"
+      }
+  },
+  {
+    name: "Ghana Away",
+    img: "vxpmRQLK/ghana-2026-away-kit.jpg",
+    opts: {
+        key: "away"
+      }
+  },
+  {
+    name: "Haiti Away",
+    img: "Vpcgq7x8/haiti-2026-away-kit.jpg",
+    opts: {
+        key: "away"
+      }
+  },
+  {
+    name: "Iran Away",
+    img: "gLs4DvR7/iran-2026-away-kit.jpg",
+    opts: {
+        key: "away"
+      }
+  },
+  {
+    name: "Iraq Away",
+    img: "Rkc248fz/iraq-2026-away-kit.jpg",
+    opts: {
+        key: "away"
+      }
+  },
+  {
+    name: "Ivory Coast Away",
+    img: "RTL5ZLQZ/ivory-coast-2026-away-kit.jpg",
+    opts: {
+        key: "away"
+      }
+  },
+  {
+    name: "Japan Away",
+    img: "Cs38sT35/japan-2026-away-kit.jpg",
+    opts: {
+        key: "away"
+      }
+  },
+  {
+    name: "Jordan Away",
+    img: "dwJMn11z/jordan-2026-away-kit.jpg",
+    opts: {
+        key: "away"
+      }
+  },
+  {
+    name: "Mexico Away",
+    img: "S4t1cHrQ/mexico-2026-away-kit.jpg",
+    opts: {
+        key: "away"
+      }
+  },
+  {
+    name: "Morocco Away",
+    img: "QFjkXZML/morocco-2026-away-kit.jpg",
+    opts: {
+        key: "away"
+      }
+  },
+  {
+    name: "Netherlands Away",
+    img: "vvKSC3n8/netherlands-2026-away-kit.jpg",
+    opts: {
+        key: "away"
+      }
+  },
+  {
+    name: "New Zealand Away",
+    img: "tpKgcm38/new-zealand-2026-away-kit.jpg",
+    opts: {
+        key: "away"
+      }
+  },
+  {
+    name: "Norway Away",
+    img: "xKg0g8HC/norway-2026-away-kit.jpg",
+    opts: {
+        key: "away"
+      }
+  },
+  {
+    name: "Panama Away",
+    img: "4nLCh4pP/panama-2026-away-kit.jpg",
+    opts: {
+        key: "away"
+      }
+  },
+  {
+    name: "Paraguay Away",
+    img: "1JL7sgpk/paraguay-2026-away-kit.jpg",
+    opts: {
+        key: "away"
+      }
+  },
+  {
+    name: "Portugal Away",
+    img: "DPhYVTWD/portugal-2026-away-kit.jpg",
+    opts: {
+        key: "away"
+      }
+  },
+  {
+    name: "Qatar Away",
+    img: "Ndzt7Hpg/qatar-2026-away-kit.jpg",
+    opts: {
+        key: "away"
+      }
+  },
+  {
+    name: "Saudi Arabia Away",
+    img: "9kMGXw9w/saudi-arabia-2026-away-kit.jpg",
+    opts: {
+        key: "away"
+      }
+  },
+  {
+    name: "Scotland Away",
+    img: "21JQsrff/scotland-2026-away-kit.jpg",
+    opts: {
+        key: "away"
+      }
+  },
+  {
+    name: "Senegal Away",
+    img: "wnG1RLy/senegal-2026-away-kit.jpg",
+    opts: {
+        key: "away"
+      }
+  },
+  {
+    name: "South Africa Away",
+    img: "23ZZCddS/south-africa-2026-away-kit.jpg",
+    opts: {
+        key: "away"
+      }
+  },
+  {
+    name: "South Korea Away",
+    img: "LydmMq0/south-korea-2026-away-kit.jpg",
+    opts: {
+        key: "away"
+      }
+  },
+  {
+    name: "Spain Away",
+    img: "ZzWwhFLP/spain-2026-away-kit.jpg",
+    opts: {
+        key: "away"
+      }
+  },
+  {
+    name: "Sweden Away",
+    img: "PvWfN3ww/sweden-2026-away-kit.jpg",
+    opts: {
+        key: "away"
+      }
+  },
+  {
+    name: "Switzerland Away",
+    img: "SX6Rt30L/switzerland-2026-away-kit.jpg",
+    opts: {
+        key: "away"
+      }
+  },
+  {
+    name: "Tunisia Away",
+    img: "0RkQNZYF/tunisia-2026-away-kit.jpg",
+    opts: {
+        key: "away"
+      }
+  },
+  {
+    name: "Turkey Away",
+    img: "8gYhncs7/turkiye-2026-away-kit.jpg",
+    opts: {
+        key: "away"
+      }
+  },
+  {
+    name: "Uruguay Away",
+    img: "hxBd9Xw9/uruguay-2026-away-kit.jpg",
+    opts: {
+        key: "away"
+      }
+  },
+  {
+    name: "USA Away",
+    img: "B2vcBp6X/usa-2026-away-kit.jpg",
+    opts: {
+        key: "away"
+      }
+  },
+  {
+    name: "Uzbekistan Away",
+    img: "gFr6Rrfj/uzbekistan-2026-away-kit.jpg",
+    opts: {
+        key: "away"
+      }
+  }
+];
